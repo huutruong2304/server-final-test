0cd326f3ce742bdc192f9cdbb4a96a5f53c22801
Nguyễn Hữu Trường <38267209+huutruong2304@users.noreply.github.com>
2020-02-22 18:29:17 +0700

    the bad day

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,16 +1,31 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
 export const typeOrmConfig:TypeOrmModuleOptions = {
-    type: 'mysql',
-    host: 'db4free.net',
+    type: 'postgres',
+    host: 'ec2-184-72-235-159.compute-1.amazonaws.com',
     // host: 'localhost',
-    port: 3306,
+    port: 5432,
     // username: 'root',
-    username: 'truongnran',
+    username: 'chhtdjrhgjlovo',
     // password:'',
-    password:'idontknow',
+    password:'e2b9121c8945efd760b7dbce89c044da9e7dd4dac1909091c203dd591ec874d9',
     // database:'test4',
-    database:'truong12345',
+    database:'ddrl7uv4gj0fpj',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
 }
+
+// export const typeOrmConfig:TypeOrmModuleOptions = {
+//     type: 'mysql',
+//     host: 'db4free.net',
+//     // host: 'localhost',
+//     port: 3306,
+//     // username: 'root',
+//     username: 'truongnran',
+//     // password:'',
+//     password:'idontknow',
+//     // database:'test4',
+//     database:'truong12345',
+//     entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//     synchronize: true
+// }
